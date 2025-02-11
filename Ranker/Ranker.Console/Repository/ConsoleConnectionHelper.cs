39b27068d9e1a116c192cd71afe37fb9ba84bb97
JonHedegaardNielsen <166702823+JonHedegaardNielsen@users.noreply.github.com>
2025-02-11 14:35:56 +0100

    made connection string

--- Ranker/Ranker.Console/Repository/ConsoleConnectionHelper.cs
+++ Ranker/Ranker.Console/Repository/ConsoleConnectionHelper.cs
@@ -5,13 +5,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Ranker.Console.Repository;
 
 internal class ConsoleConnectionHelper : IConnectionHelper<SQLiteConnection>
 {
+	public string DatabaseName => "database.db";
 	public SQLiteConnection GetConnection()
 	{
-		return new SQLiteConnection("Data Source=database.db");
+		SQLiteConnection conn = new ($"Data Source={DatabaseName}");
+		conn.Open();
+		return conn;
+	}
+
+	public void ExecuteNonQuery(string sql)
+	{
+		using SQLiteConnection connection = GetConnection();
+		SQLiteCommand cmd = connection.CreateCommand();
+		cmd.CommandText = sql;
+		cmd.ExecuteNonQuery();
 	}
 }
