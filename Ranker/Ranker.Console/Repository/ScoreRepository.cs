c2c1c920609cb4ec73b24a79dd6d963e4749eacb
ShaqTheSnack <reichsteinshaquille@gmail.com>
2025-02-14 12:28:58 +0100

    ScoreRepsitory Rework

--- Ranker/Ranker.Console/Repository/ScoreRepository.cs
+++ Ranker/Ranker.Console/Repository/ScoreRepository.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,23 +14,20 @@ public class ScoreRepository : IRepository<Score>
 {
     IConnectionHelper<SQLiteConnection> _connectionHelper;
     const string TABLE = "score";
-    private readonly CommonDBModules<Score> _commonDBModules;
 
     public ScoreRepository(IConnectionHelper<SQLiteConnection> connectionHelper)
     {
         _connectionHelper = connectionHelper;
-        _commonDBModules = new(_connectionHelper);
     }
 
     private Score ReadRow(SQLiteDataReader reader)
     {
-        return new()
+        return new Score()
         {
             ScoreId = reader.GetInt32(0),
             ScoreBoardId = reader.GetInt32(1),
             ParticipantName = reader.GetString(2),
-            Points = reader.GetInt32(3),
-            Timer = reader.GetFloat(4)
+            Points = reader.GetInt32(3)
         };
     }
 
@@ -38,12 +36,11 @@ public class ScoreRepository : IRepository<Score>
         using SQLiteConnection conn = _connectionHelper.GetConnection();
         SQLiteCommand cmd = conn.CreateCommand();
         cmd.CommandText = $@"
-        INSERT INTO {TABLE} (participantName, points, timer, scoreBoardId) 
-        VALUES (@participantName, @points, @timer, @scoreBoardId)";
+        INSERT INTO {TABLE} (participantName, points, scoreBoardId) 
+        VALUES (@participantName, @points, @scoreBoardId)";
 
         cmd.Parameters.AddWithValue("@participantName", model.ParticipantName);
         cmd.Parameters.AddWithValue("@points", model.Points);
-        cmd.Parameters.AddWithValue("@timer", model.Timer);
         cmd.Parameters.AddWithValue("@scoreBoardId", model.ScoreBoardId);
 
         cmd.ExecuteNonQuery();
@@ -64,9 +61,30 @@ public class ScoreRepository : IRepository<Score>
 
 	public IEnumerable<Score> GetAll()
 	{
-        return _commonDBModules.ExecuteQuery($@" 
+        using SQLiteConnection conn = _connectionHelper.GetConnection();
+        SQLiteCommand cmd = conn.CreateCommand();
+        cmd.CommandText = $@" 
         SELECT scoreId, participantName, points, timer, scoreBoardId
-        FROM {TABLE}", ReadRow);
+        FROM {TABLE}";
+
+
+
+        using SQLiteDataReader reader = cmd.ExecuteReader();
+        List<Score> scores = new();
+
+        while (reader.Read())
+        {
+            scores.Add(new Score
+            {
+                ScoreId = reader.GetInt32(0),
+                ScoreBoardId = reader.GetInt32(1),
+                ParticipantName = reader.GetString(2),
+                Points = reader.GetInt32(3)
+            });
+        }
+
+        return scores;
+
     }
 
 	public Score? GetById(int id)
@@ -79,7 +97,21 @@ public class ScoreRepository : IRepository<Score>
         WHERE scoreId = @id";
         cmd.Parameters.AddWithValue("@id", id);
 
-        return _commonDBModules.ExecuteSingleQuery(cmd, ReadRow);
+        using SQLiteDataReader reader = cmd.ExecuteReader();
+        List<Score> scores = new();
+
+        if (reader.Read())
+        {
+            return new Score
+            {
+                ScoreId = reader.GetInt32(0),
+                ScoreBoardId = reader.GetInt32(1),
+                ParticipantName = reader.GetString(2),
+                Points = reader.GetInt32(3)
+            };
+        }
+
+        return null;
     }
 
 	public void Update(Score model)
@@ -88,15 +120,15 @@ public class ScoreRepository : IRepository<Score>
         SQLiteCommand cmd = conn.CreateCommand();
         cmd.CommandText = $@"
         UPDATE {TABLE} 
-        SET participantName = @participantName, points = @points, timer = @timer
+        SET participantName = @participantName, points = @points
         WHERE scoreId = @scoreId";
 
 
         cmd.Parameters.AddWithValue("@participantName", model.ParticipantName);
         cmd.Parameters.AddWithValue("@points", model.Points);
-        cmd.Parameters.AddWithValue("@timer", model.Timer);
         cmd.Parameters.AddWithValue("@scoreId", model.ScoreId);
 
         cmd.ExecuteNonQuery();
+
     }
 }
