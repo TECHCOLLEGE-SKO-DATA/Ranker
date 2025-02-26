5d67a293f7d5a941980a79f82235a6ca58b8e3bb
JonHedegaardNielsen <166702823+JonHedegaardNielsen@users.noreply.github.com>
2025-02-26 09:19:12 +0100

    userinterface class made

--- Ranker/Ranker.Console/Program.cs
+++ Ranker/Ranker.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Ranker.Console.Repository;
+using Ranker.Console.UserInterface;
 using Ranker.Lib.Models;
 using System.Data.SQLite;
 
@@ -33,71 +34,8 @@ class Program
             );");
         }
 
-        ScoreBoardRepository scoreBoardRepo = new(_connHelper);
-		ScoreRepository scoreRepo = new(_connHelper);
-        try
-        {
-			scoreBoardRepo.Add(new ScoreBoard
-			{
-				Name = "1",
-				Description = "2",
-				UniqueKey = "11111111",
-				SettingId = 1
-			});
-		}
-        catch
-        {
-
-        }
-
-        try
-        {
-			scoreRepo.Add(new()
-			{
-				Points = 5,
-				ParticipantName = "Shaq",
-				ScoreBoardId = 1,
-			});
-		}
-        catch
-        {
-
-        }
-        
-
-        var data = (List<ScoreBoard>)scoreBoardRepo.GetAll();
-
-        for (int i = 0; i < data.Count(); i++)
-        {
-            Console.WriteLine($"nr.{i} : {data[i].Name}");
-        }
-
-        string? userInput = "";
-        int userInputNumber = 0;
-
-        do
-        {
-            Console.Write("intast nummer på scoreboard du vil se: ");
-            userInput = Console.ReadLine();
-        } while (userInput == null && int.TryParse(userInput, out userInputNumber) && userInputNumber >= 0 && userInputNumber < data.Count());
-
-        ScoreBoard pickedScoreboard = data[userInputNumber];
-
-        Console.Clear();
-        string scoreboardText = "-----------------------------------\n";
-
-        var scores = scoreRepo.GetFromScoreBoardId(pickedScoreboard.ScoreBoardId);
-
-        foreach (Score score in scores)
-        {
-            scoreboardText += $"| {score.ParticipantName} | {score.Points} |\n" +
-            "----------------------------------------------------\n";
-            
-        }
-
-        Console.WriteLine(scoreboardText);
-
-        Console.ReadLine();
+        UserInterface userInterface = new UserInterface();
+        userInterface.Draw();
     }
 }
 
---
+++ Ranker/Ranker.Console/UserInterface/UserInterface.cs
@@ -0,0 +1,78 @@
+﻿using Ranker.Console.Repository;
+using Ranker.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranker.Console.UserInterface;
+
+class UserInterface
+{
+	ConsoleConnectionHelper _connectionHelper = new();
+	ScoreBoardRepository scoreBoardRepo;
+	ScoreRepository scoreRepo;
+
+	public UserInterface()
+	{
+		scoreRepo = new(_connectionHelper);
+		scoreBoardRepo = new(_connectionHelper);
+	}
+
+	void DrawScoreBoards()
+	{
+
+		var data = (List<ScoreBoard>)scoreBoardRepo.GetAll();
+
+		for (int i = 0; i < data.Count(); i++)
+		{
+			System.Console.WriteLine($"nr.{i} : {data[i].Name}");
+		}
+
+		string? userInput = "";
+		int userInputNumber = 0;
+
+		do
+		{
+			System.Console.WriteLine("intast nummer på scoreboard du vil se: ");
+			userInput = System.Console.ReadLine();
+		} while (userInput == null && int.TryParse(userInput, out userInputNumber) && userInputNumber >= 0 && userInputNumber < data.Count());
+
+		DrawScores(data[userInputNumber]);
+	}
+
+	void DrawScores(ScoreBoard scoreBoard)
+	{
+		System.Console.WriteLine();
+		string scoreboardText = "-----------------------------------\n";
+
+		var scores = scoreRepo.GetFromScoreBoardId(scoreBoard.ScoreBoardId);
+
+		foreach (Score score in scores)
+		{
+			scoreboardText += $"| {score.ParticipantName} | {score.Points} |\n" +
+			"----------------------------------------------------\n";
+
+		}
+
+		System.Console.WriteLine(scoreboardText);
+
+		switch (System.Console.ReadLine().ToLower())
+		{
+			case "add":
+				CreateScore();
+				break;
+		}
+	}
+
+	public void Draw()
+	{
+		DrawScoreBoards();
+	}
+
+	void CreateScore()
+	{
+		
+	}
+}
