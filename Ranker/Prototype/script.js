ee6091388558fa53211682f870d91d99e36a7092
ShaqTheSnack <reichsteinshaquille@gmail.com>
2025-03-12 09:17:44 +0100

    yes

--- Ranker/Prototype/script.js
+++ Ranker/Prototype/script.js
@@ -2,9 +2,10 @@ let selectedScoreBoardElement
 
 const scoreBoardTableBody = document.getElementById("scoreBoardTableBody")
 const scoreBoardTable = document.getElementById("scoreBoardTable")
+const scoreTable = document.getElementById("scoreTable")
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const response = await fetch('https://localhost:7285/ScoreBoard', {
+    const scoreBoardResponse = await fetch('https://localhost:7285/ScoreBoard', {
         method: "GET",
         headers: {
             "Content-Type": "application/json"
@@ -12,16 +13,20 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     });
 
-    const data = await response.json()
 
-    const dataArray = Array.isArray(data) ? data : [data];
 
-    dataArray.forEach((element) => {
+
+    const scoreBoarddata = await scoreBoardResponse.json()
+
+    /*const dataArray = Array.isArray(scoreBoarddata) ? scoreBoarddata : [scoreBoarddata];*/
+    const scoreBoardDataArray = [].concat(scoreBoarddata);
+
+    scoreBoardDataArray.forEach((element) => {
 
         const tr = document.createElement("tr")
-        tr.addEventListener("click", () => selectScoreBoard(tr)) 
+        tr.addEventListener("click", () => selectScoreBoard(tr))
         tr.innerHTML = `
-            <td>${element.scoreBoardId}</td>
+            <td class="scoreBoardId">${element.scoreBoardId}</td>
             <td>${element.name}</td>
             <td>${element.description}</td>
             <td>${element.settingId}</td>
@@ -30,47 +35,73 @@ document.addEventListener("DOMContentLoaded", async () => {
         scoreBoardTable.appendChild(tr)
 
     })
+
 })
 
-function selectScoreBoard(element){
-    if (selectedScoreBoardElement == element){
+async function selectScoreBoard(element) {
+    if (selectedScoreBoardElement == element) {
 
         selectedScoreBoardElement = null
         element.classList.remove("selected")
+        console.log("MABYE")
     }
-    else{
-        if (selectedScoreBoardElement != null){
+    else {
+        if (selectedScoreBoardElement != null) {
             selectedScoreBoardElement.classList.remove("selected")
-        } 
-        element.classList.add("selected")
+            console.log("YES")
+        }
+
+
+        element.classList.add("selected.scoreBoardId")
         selectedScoreBoardElement = element
+        scoreTable.innerHTML = ""
+        const scoreBoardIdElement = element.querySelector(".scoreBoardId")
+        const scoreResponse = await fetch(`https://localhost:7285/Score/getById/${scoreBoardIdElement.innerHTML}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+
+        });
+        const scoreData = await scoreResponse.json()
+
+
+        const scoreDataArray = [].concat(scoreData);
+        console.log(scoreDataArray)
+        scoreDataArray.forEach((element) => {
+
+            const tr = document.createElement("tr")
+            tr.addEventListener("click", () => selectScoreBoard(tr))
+            tr.innerHTML = `
+            <td>${element.scoreId}</td>
+            <td>${element.participantName}</td>
+            <td>${element.points}</td>
+        `
+            scoreTable.appendChild(tr)
+
+        })
     }
 }
 
-function ScoreboardAdd()
-{
+function ScoreboardAdd() {
 }
 
-function ScoreboardUpdate()
-{
+function ScoreboardUpdate() {
 }
 
-function ScoreboardDelete()
-{
+function ScoreboardDelete() {
 }
 
-function ScoreboardEnter()
-{
+function ScoreboardEnter() {
 }
 
 
-function ScoreAdd()
-{
+function ScoreAdd() {
 }
 
-function ScoreUpdate()
-{
+function ScoreUpdate() {
 }
 
-function ScoreDelete()
-{
+function ScoreDelete() {
+
+}
